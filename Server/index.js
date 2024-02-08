a9f963f13284b0cf2379b79be0d05e67071091f6
TonySMK <tonysun25light@live.ca>
2024-02-07 22:48:42 -0500

    added express ands to dependencies and added possible route files

--- Server/index.js
+++ Server/index.js
@@ -1,27 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
 const cors = require("cors");
-const path = require('path');
+// const path = require('path');
+const userPage = require("./routes/userpage_route");
+const paymentPage = require("./routes/paymentpage_route");
 
 app.use(cors());
 app.use(express.json());
 
-
 app.use((req, res, next) => {
-    console.log("Link validation middleware");
-    next(); 
-})
+  console.log("Link validation middleware");
+  next();
+});
 
-
-
-
-
-app.get('/', (req, res) => {
-    res.send("Welcome to the visual world");
-})
-
+app.get("/", (req, res) => {
+  res.send("Welcome to the visual world");
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
