81a775ff9b016800ce2098310a59619a82988722
Bryce Borer <bryce.borer@gmail.com>
2024-02-10 12:23:28 -0500

    database is built

---
+++ Server/migrations/20240210164200_create.js
@@ -0,0 +1,38 @@
+// Easy Drop Block
+exports.up = function (knex) {
+    return knex.schema
+  .then(() => {
+    // CREATE BASIC TABLES
+    // Table to hold all menu items for the entire restaurant
+    return knex.schema.createTable('menu_items', function(table) {
+      table.increments('item_id').primary();
+      table.string('item_name', 60).notNullable();
+      table.string('item_description', 200);
+      table.decimal('price', 10, 2);
+    });
+  })
+  .then(() => {
+    // Table to store new/returning customers along with key info for app use
+    return knex.schema.createTable('customers', function(table) {
+      table.increments('customer_id').primary();
+      table.string('customer_name', 30);
+      table.string('preferred_payment_method', 30);
+      table.string('email', 50);
+    });
+  })
+  .then(() => {
+    // Table to house the items ordered for a particular physical table in the service area
+    return knex.schema.createTable('seating', function(table) {
+      table.increments('order_id').primary();
+      table.integer('menu_item_id').unsigned();
+      table.integer('ordering_party').unsigned();
+      table.string('special_instructions', 60);
+      table.foreign('menu_item_id').references('menu_items.item_id').onDelete('CASCADE');
+      table.foreign('ordering_party').references('customers.customer_id').onDelete('CASCADE');
+    });
+  })
+}
+
+  exports.down = function (knex) {
+    return knex.schema.dropTable("menu_items").dropTable("customers").dropTable("seating");
+  };
---
+++ Server/seed-data/seating.js
@@ -0,0 +1,59 @@
+module.exports = 
+[
+    {
+      menu_item_id: 2,
+      ordering_party: 1,
+      special_instructions: null
+    },
+    {
+      menu_item_id: 5,
+      ordering_party: 1,
+      special_instructions: null
+    },
+    {
+      menu_item_id: 18,
+      ordering_party: 1,
+      special_instructions: "Sprite"
+    },
+    {
+      menu_item_id: 10,
+      ordering_party: 2,
+      special_instructions: "Regular Mayo"
+    },
+    {
+      menu_item_id: 18,
+      ordering_party: 2,
+      special_instructions: "Diet Coke"
+    },
+    {
+      menu_item_id: 6,
+      ordering_party: 3,
+      special_instructions: null
+    },
+    {
+      menu_item_id: 1,
+      ordering_party: 3,
+      special_instructions: "No Avocado"
+    },
+    {
+      menu_item_id: 19,
+      ordering_party: 3,
+      special_instructions: "Unsweet"
+    },
+    {
+      menu_item_id: 8,
+      ordering_party: 4,
+      special_instructions: null
+    },
+    {
+      menu_item_id: 4,
+      ordering_party: 4,
+      special_instructions: null
+    },
+    {
+      menu_item_id: 18,
+      ordering_party: 4,
+      special_instructions: "Sprite"
+    }
+  ]
+  
