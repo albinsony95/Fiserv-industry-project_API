04093b0e346f8877eb47743c302f93c1009c074a
Bryce Borer <bryce.borer@gmail.com>
2024-02-11 05:11:53 -0500

    added item_total to review response. Helpful when someone orders more than 1 of an item

--- Server/controllers/reviewController.js
+++ Server/controllers/reviewController.js
@@ -7,21 +7,23 @@ const index = (_req, res) => {
       'order.menu_item_id',
       'menu_items.item_name',
       'menu_items.price',
-      'menu_items.item_image',
       'order.order_quantity',
+      // Calculate the item_total
+      knex.raw('?? * ?? as item_total', ['menu_items.price', 'order.order_quantity']),
+      'menu_items.item_image', 
       'order.ordering_party',
       'customers.customer_name',
-      'order.special_instructions'
+      'order.special_instructions',
     )
-    .join('menu_items', 'order.menu_item_id', '=', 'menu_items.item_id') // Ensure correct table names and join conditions
-    .join('customers', 'order.ordering_party', '=', 'customers.customer_id') // Ensure correct table names and join conditions
+    // Ensure correct table names and join conditions
+    .join('menu_items', 'order.menu_item_id', '=', 'menu_items.item_id') 
+    .join('customers', 'order.ordering_party', '=', 'customers.customer_id')
     .then((data) => {
       res.status(200).json(data);
     })
     .catch((err) => {
       res.status(400).send(`Error retrieving Receipt Review: ${err}`)
     });
-
 }
 
 module.exports = {
